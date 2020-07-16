9ff2186500ab6b22997f329e586f81342fd4f45a
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-16 23:59:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
-def prime?(num)
-  if -1 > num
-    return false
-  end
-  i = 2
-  while i < num
-    if num % i == 0
-      return false
-    end 
-  i += 1
-  end
-  true
-end
+# def prime?(num)
+#   if -1 > num
+#     return false
+#   end
+#   i = 2
+#   while i < num
+#     if num % i == 0
+#       return false
+#     end 
+#   i += 1
+#   end
+#   true
+# end
