400358c9708059324216a84f84d4e1a10362e047
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-16 23:59:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,3 +11,16 @@
 #   end
 #   true
 # end
+
+ef prime?(value)
+  if value <= 1 || value == 0 || value == 1
+    return false
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
+        return false
+      end
+    end
+  end
+  true  # <= need explanation
+end
