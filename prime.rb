b565670e582a3254d7117cbaf67b4f9f011c90c0
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-17 00:06:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(num)
-#   if num <= 1 || num == 0 || num == 1
-#     return false
-#   elsif
-#     (2..num - 1).each do |i|
-#       if value % i == 0
-#         return false
-#       end
-#     end
-#   end
-#   true  
-# end
+  if num <= 1 || num == 0 || num == 1
+    return false
+  elsif
+    (2..num - 1).each do |i|
+      if value % i == 0
+        return false
+      end
+    end
+  end
+  true  
+end
